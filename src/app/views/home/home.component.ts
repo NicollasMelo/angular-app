e4c86aea63373248d2ac46b3a55365264c889792
NicollasMelo <nicollasmelo2001@gmail.com>
2024-01-09 12:43:05 -0300

    feat: inserindo cards e configurando responsividade

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { LiveListComponent } from './live-list/live-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    LiveListComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
---
+++ src/app/views/home/live-list/live-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-live-list',
+  standalone: true,
+  imports: [MatTabsModule, MatCardModule],
+  templateUrl: './live-list.component.html',
+  styleUrl: './live-list.component.css',
+})
+export class LiveListComponent {}
